a6f11038f1e99381a44b6c6545cdf1a52f70c469
KenJamesHioki <kenj.hioki@gmail.com>
2024-03-10 17:34:38 +0900

    Udemyコースの必要ファイルを全てコミット

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hero = new HeroSlider('.swiper');
+  hero.start();
+
+  // const ta = new TweenTextAnimation('.tween-animate-title');
+  // ta.animate();
+
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  }
+
+  const so = new ScrollObserver('.tween-animate-title', cb);
+
+
+  const cb2 = function (el, isIntersecting) {
+    if (isIntersecting) {
+      el.classList.add('inview')
+    } else {
+      el.classList.remove('inview')
+
+    }
+  }
+
+  const so2 = new ScrollObserver('.cover-slide', cb2);
+});
+
